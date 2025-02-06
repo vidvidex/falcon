fe0a07cc9e44f2b82aefc56de7290224f9595853
Vid Smole <vid@smole.org>
2025-02-06 17:03:42 +0100

    Implement INTT

--- falcon.srcs/sim_1/new/ntt_tb.sv
+++ falcon.srcs/sim_1/new/ntt_tb.sv
@@ -5,6 +5,7 @@ module ntt_tb;
   logic clk;
   logic rst_n;
 
+  logic mode;
   logic start;
   logic [14:0] input_polynomial[0:7];
   logic [14:0] twiddle_factors[0:7];
@@ -18,6 +19,7 @@ module ntt_tb;
       )uut(
         .clk(clk),
         .rst_n(rst_n),
+        .mode(mode),
         .start(start),
         .input_polynomial(input_polynomial),
         .done(done),
@@ -29,12 +31,12 @@ module ntt_tb;
   initial begin
     clk = 1;
 
-    // Test 1: Polynomial of size 8
+    // Test 1: NTT of polynomial size 8
     rst_n = 0;
     #10;
     rst_n = 1;
+    mode = 0;
     input_polynomial = {0,1,2,3,4,5,6,7};
-    start = 0;
 
     // Start NTT
     start = 1;
@@ -51,6 +53,29 @@ module ntt_tb;
       $display("Test 1: Passed");
     else
       $display("Test 1: Failed");
+
+    // Test 2: INTT of polynomial size 8
+    rst_n = 0;
+    #10;
+    rst_n = 1;
+    mode = 1;
+    input_polynomial = {28,10781,6369,10324,12285,1957,5912,1500};
+
+    // Start INTT
+    start = 1;
+    #10;
+    start = 0;
+
+    // Wait for INTT to finish
+    while (!done)
+      #10;
+
+    // Check output
+    expected_output_polynomial = {0,1,2,3,4,5,6,7};
+    if (output_polynomial === expected_output_polynomial)
+      $display("Test 2: Passed");
+    else
+      $display("Test 2: Failed");
 
     $finish;
   end
--- falcon.srcs/sources_1/new/ntt.sv
+++ falcon.srcs/sources_1/new/ntt.sv
@@ -1,7 +1,11 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 //
-// Converts polynomial from coefficient form to NTT form.
+// Implements both NTT and inverse NTT, because we are never going to use both at
+// the same time, therefore we can save resources by reusing the same module.
+//
+//  Forward NTT: IDLE -> COPY_BIT_REVERSED -> NTT -> COPY(done=1) -> IDLE
+//  Inverse NTT: IDLE -> COPY -> NTT -> COPY_BIT_REVERSED(done=1) -> IDLE
 //
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -11,6 +15,8 @@ module ntt#(
   )(
     input logic clk,
     input logic rst_n,
+
+    input logic mode, // 0: NTT, 1: Inverse NTT
 
     input logic start, //! Polynomial is valid, NTT can start
     input logic [14:0] input_polynomial[0:N-1], //! Polynomial in coefficient form
@@ -29,14 +35,15 @@ module ntt#(
 
   typedef enum logic [1:0] {
             IDLE,   // Waiting for start signal
-            COPY_BIT_REVERSED, // Copy input polynomial to intermediate storage in bit-reversed order
-            NTT, // Perform NTT
-            DONE  // NTT is done, copy polynomial to output
+            COPY_BIT_REVERSED, // Copy input polynomial to intermediate intermediate or intermediate polynomial to output while reversing the order of the coefficients
+            COPY, // Copy input polynomial to intermediate intermediate or intermediate polynomial to output
+            NTT // Perform NTT/INTT
           } state_t;
   state_t state, next_state;
 
   // Instantiate twiddle factor ROM
   twiddle_rom #(.N(N)) twiddle_rom (
+                .mode(mode),
                 .addr(address),
                 .data(twiddle_factor)
               );
@@ -92,20 +99,40 @@ module ntt#(
     case (state)
       IDLE: begin   // Waiting for the start signal
         if (start == 1'b1)
-          next_state <= COPY_BIT_REVERSED;
 
+          // For NTT we have to bit reverse now, for INTT we do it at the end
+          if(mode == 1'b0)
+            next_state <= COPY_BIT_REVERSED;
+          else
+            next_state <= COPY;
       end
-      COPY_BIT_REVERSED: begin // Copy input polynomial to intermediate storage in bit-reversed order. This takes one cycle, so we continue directly to NTT
-        next_state <= NTT;
+      COPY_BIT_REVERSED: begin // Copy input polynomial to intermediate storage or from intermediate storage to output polynomial while reversing the order of the coefficients. This takes just one cycle
+
+        // For NTT we go to the NTT state, while for INTT we go to the IDLE state (we reverse at the end)
+        if(mode == 1'b0)
+          next_state <= NTT;
+        else
+          next_state <= IDLE;
       end
-      NTT: begin
-        if (stage == $clog2(N)-1 && butterfly >= N/2-2) // Set next_state when we are at the second to last element of the last stage (this accounts for the delay in switching states)
-          next_state <= DONE;
+      COPY: begin // Copy input polynomial to intermediate storage or from intermediate storage to output polynomial. This takes just one cycle
+
+        // For NTT we go to the IDLE state, while for INTT we go to the NTT state
+        if(mode == 1'b0)
+          next_state <= IDLE;
         else
           next_state <= NTT;
       end
-      DONE: begin
-        next_state <= IDLE;
+      NTT: begin
+        if (stage == $clog2(N)-1 && butterfly >= N/2-2) begin // Set next_state when we are at the second to last element of the last stage (this accounts for the delay in switching states)
+
+          // For NTT we just copy, for INTT we have to copy and bit reverse
+          if(mode == 1'b0)
+            next_state <= COPY;
+          else
+            next_state <= COPY_BIT_REVERSED;
+        end
+        else
+          next_state <= NTT;
       end
       default: begin
         next_state <= IDLE;
@@ -119,9 +146,16 @@ module ntt#(
         done = 0;
       end
       COPY_BIT_REVERSED: begin
-        done = 0;
 
-        // Copy coefficients from input to intermediate storage and reverse indices
+        // For INTT this is the last state
+        if(mode == 1'b0)
+          done = 0;
+        else
+          done = 1;
+
+        // Copy coefficients from input to intermediate storage or from intermediate storage to output
+        // while reversing the order of the coefficients
+
         // at which they are stored (000 -> 000, 001 -> 100, 010 -> 010, 011 -> 110, ...)
         // Example for N=8:
         // polynomial[3'b000] = input_polynomial[3'b000];
@@ -131,21 +165,33 @@ module ntt#(
         // polynomial[3'b111] = input_polynomial[3'b111];
         for (index = 0; index < N; index = index + 1) begin
           index_rev = {<<{index}};  // index and index_rev should be [log(N)-1:0] but are [log(N):0] because with the one bit shorter the simulation doesn't start (it's just waiting). For this reason we have the >>1 in the next line to correct the index_rev
-          polynomial[index] = input_polynomial[index_rev>>1];
-        end
 
+          if(mode == 1'b0)
+            polynomial[index_rev>>1] = input_polynomial[index];
+          else
+            output_polynomial[index] = polynomial[index_rev>>1]>> $clog2(N);  // For INTT we have to scale down the coefficients by N
+        end
+      end
+
+      COPY: begin
+
+        // For NTT this is the last state
+        if(mode == 1'b0)
+          done = 1;
+        else
+          done = 0;
+
+        // Copy coefficients from input to intermediate storage or from intermediate storage to output
+        for (index = 0; index < N; index = index + 1) begin
+          if(mode == 1'b0)
+            output_polynomial[index] = polynomial[index];
+          else
+            polynomial[index] = input_polynomial[index];
+        end
       end
 
       NTT: begin
         done = 0;
-      end
-
-      DONE: begin
-        // Copy polynomial from intermediate storage to output
-        for (index = 0; index < N; index = index + 1) begin
-          output_polynomial[index] = polynomial[index];
-        end
-        done = 1;
       end
 
       default: begin
@@ -161,8 +207,14 @@ module ntt#(
       stage <= 0;
       butterfly <= 0;
 
-      stride <= 1;  // Offset between indices of elements in the same stage
-      address_stride <= $clog2(N<<1); // How far to jump in the twiddle factor ROM (stage 1: N/2, stage 2: N/4, ...)
+      if(mode == 1'b0) begin
+        stride <= 1;        // Initialize stride (offset between indices of elements in the same stage)
+        address_stride <= $clog2(N<<1); // How far to jump in the twiddle factor ROM (stage 1: N/2, stage 2: N/4, ...)
+      end
+      else begin
+        stride <= N >> 1;
+        address_stride <= 1;
+      end
 
       address <= 0;
 
@@ -177,8 +229,15 @@ module ntt#(
       // Increment stage when we have processed all elements in the current stage
       if (butterfly == N/2-1) begin
         stage <= stage + 1;
-        stride <= stride << 1;
-        address_stride <= address_stride >> 1;
+
+        if(mode == 1'b0) begin
+          stride <= stride << 1;
+          address_stride <= address_stride >> 1;
+        end
+        else begin
+          stride <= stride >> 1;
+          address_stride <= address_stride << 1;
+        end
         butterfly <= 0;
       end
       else
@@ -193,9 +252,14 @@ module ntt#(
         i <= i + 1;
 
       // Butterfly operation
-      polynomial[i] <= mod_add(polynomial[i], mod_mult(polynomial[i + stride], twiddle_factor));
-      polynomial[i + stride] <= mod_sub(polynomial[i], mod_mult(polynomial[i + stride], twiddle_factor));
-
+      if (mode == 1'b0) begin
+        polynomial[i] <= mod_add(polynomial[i], mod_mult(polynomial[i + stride], twiddle_factor));
+        polynomial[i + stride] <= mod_sub(polynomial[i], mod_mult(polynomial[i + stride], twiddle_factor));
+      end
+      else begin
+        polynomial[i] <= mod_add(polynomial[i], polynomial[i + stride]);
+        polynomial[i + stride] <= mod_mult( mod_sub(polynomial[i], polynomial[i + stride]), twiddle_factor);
+      end
     end
   end
 endmodule
