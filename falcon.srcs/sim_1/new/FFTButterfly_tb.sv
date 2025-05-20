3be4252f141d19cc20a870b2c0ba132b2a2bc4d6
Vid Smole <vid@smole.org>
2025-05-20 20:34:45 +0200

    Implement pipeline ability to FFTButterfly (twiddle factor changing not supported yet)

---
+++ falcon.srcs/sim_1/new/FFTButterfly_tb.sv
@@ -0,0 +1,122 @@
+`timescale 1ns / 1ps
+`include "CommonDefinitions.vh"
+
+module FFTButterfly_tb;
+
+  logic clk;
+  logic start, done, use_ct;
+  logic [`OVERALL_BITS-1:0] a_in_real;
+  logic [`OVERALL_BITS-1:0] a_in_imag;
+  logic [`OVERALL_BITS-1:0] b_in_real;
+  logic [`OVERALL_BITS-1:0] b_in_imag;
+
+  logic [`OVERALL_BITS-1:0] tw_real;
+  logic [`OVERALL_BITS-1:0] tw_imag;
+
+  logic [`OVERALL_BITS-1:0] a_out_real;
+  logic [`OVERALL_BITS-1:0] a_out_imag;
+  logic [`OVERALL_BITS-1:0] b_out_real;
+  logic [`OVERALL_BITS-1:0] b_out_imag;
+
+
+  always #5 clk = ~clk;
+
+  FFTButterfly FFTButterfly(
+                 .clk(clk),
+                 .start(start),
+                 .use_ct(use_ct),
+
+                 .a_in_real(a_in_real),
+                 .a_in_imag(a_in_imag),
+                 .b_in_real(b_in_real),
+                 .b_in_imag(b_in_imag),
+
+                 .tw_real(tw_real),
+                 .tw_imag(tw_imag),
+
+                 .a_out_real(a_out_real),
+                 .a_out_imag(a_out_imag),
+                 .b_out_real(b_out_real),
+                 .b_out_imag(b_out_imag),
+                 .done(done)
+               );
+
+  initial begin
+
+    clk = 1;
+    start = 0;
+    use_ct = 0;
+    a_in_real = 0;
+    a_in_imag = 0;
+    b_in_real = 0;
+    b_in_imag = 0;
+    tw_real = 0;
+    tw_imag = 0;
+    #10;
+
+    start = 1;
+    use_ct = 1; // FFT
+    a_in_real = $realtobits(1.0);
+    a_in_imag = $realtobits(1.0);
+    b_in_real = $realtobits(2.0);
+    b_in_imag = $realtobits(2.0);
+    tw_real = $realtobits(1.0);
+    tw_imag = $realtobits(0.0);
+    #10;    // output: 3+3i, -1-1i
+
+    start = 1;
+    use_ct = 1; // FFT
+    a_in_real = $realtobits(12.5); 
+    a_in_imag = $realtobits(2.7);
+    b_in_real = $realtobits(1.5); 
+    b_in_imag = $realtobits(-4.2);
+    tw_real = $realtobits(1.0);
+    tw_imag = $realtobits(0.0);
+    #10;    // output: 14.0-1.5i, 11.0+6.9i
+
+    start = 1;
+    use_ct = 1; // FFT
+    a_in_real = $realtobits(2.0); 
+    a_in_imag = $realtobits(2.0);
+    b_in_real = $realtobits(4.0); 
+    b_in_imag = $realtobits(4.0);
+    tw_real = $realtobits(1.0);
+    tw_imag = $realtobits(0.0);
+    #10;    // output: 6+6i, -2-2i
+
+    // start = 1;
+    // use_ct = 0; // IFFT
+    // a_in_real = $realtobits(1.0);
+    // a_in_imag = $realtobits(1.0);
+    // b_in_real = $realtobits(2.0);
+    // b_in_imag = $realtobits(2.0);
+    // tw_real = $realtobits(1.0);
+    // tw_imag = $realtobits(0.0);
+    // #10;    // output: 3+3i; -1-1i -----needs to be scaled by 1/N----> 1.5+1.5i, -0.5-0.5i
+        
+    // start = 1;
+    // use_ct = 0; // IFFT
+    // a_in_real = $realtobits(1.0);
+    // a_in_imag = $realtobits(2.3);
+    // b_in_real = $realtobits(4.5);
+    // b_in_imag = $realtobits(5.0);
+    // tw_real = $realtobits(1.0);
+    // tw_imag = $realtobits(0.0);
+    // #10;    // output: 5.5+7.3i; -3.5-2.7i -----needs to be scaled by 1/N----> 2.75+3.65i, -1.75-1.35i
+    
+    // start = 1;
+    // use_ct = 0; // IFFT
+    // a_in_real = $realtobits(5.0);
+    // a_in_imag = $realtobits(5.0);
+    // b_in_real = $realtobits(4.0);
+    // b_in_imag = $realtobits(4.0);
+    // tw_real = $realtobits(1.0);
+    // tw_imag = $realtobits(0.0);
+    // #10;    // output: 9+9i; 1+1i -----needs to be scaled by 1/N----> 4.5+4.5i, 0.5+0.5i
+
+    start = 0;
+
+
+  end
+
+endmodule
--- falcon.srcs/sources_1/new/FFTButterfly.sv
+++ falcon.srcs/sources_1/new/FFTButterfly.sv
@@ -1,7 +1,6 @@
 `timescale 1ns / 1ps
 `include "CommonDefinitions.vh"
 
-// performs GS-FFT butterfly operation
 //  a_in_real + j*a_in_imag ----\-/|+|-- a_out_real + j*a_out_imag
 //                               X
 //  b_in_real + j*b_in_imag ----/-\|-|--|x|- b_out_real + j*b_out_imag
@@ -12,7 +11,7 @@
 module FFTButterfly(
     input clk,
     input start,
-    input use_ct,
+    input use_ct, // 0 = DIT = IFFT; 1 = DIF = FFT
     input [`OVERALL_BITS-1:0] a_in_real,
     input [`OVERALL_BITS-1:0] a_in_imag,
     input [`OVERALL_BITS-1:0] b_in_real,
@@ -30,54 +29,83 @@ module FFTButterfly(
   );
 
   /////////////////////// Add stage //////////////////////
-
   logic [`OVERALL_BITS-1:0] a_m_b_real_1DP, a_m_b_imag_1DP, mul_out_real_1DP, mul_out_imag_1DP;
   logic [`OVERALL_BITS-1:0] a_m_b_real, a_m_b_imag;
   logic add_done;
 
+  // For FFT we have to delay a_in_real and a_in_imag before it enters the add stage (pipelining doesn't work without it)
+  logic [`OVERALL_BITS-1:0] a_real, a_imag;
+  logic [`OVERALL_BITS-1:0] a_in_real_15D, a_in_imag_15D;
+  DelayRegister #(.BITWIDTH(`OVERALL_BITS), .CYCLE_COUNT(1+15)) a_in_real_delay(.clk(clk), .in(a_in_real), .out(a_in_real_15D));
+  DelayRegister #(.BITWIDTH(`OVERALL_BITS), .CYCLE_COUNT(1+15)) a_in_imag_delay(.clk(clk), .in(a_in_imag), .out(a_in_imag_15D));
+
+  assign a_real = use_ct ? a_in_real_15D : a_in_real;
+  assign a_imag = use_ct ? a_in_imag_15D : a_in_imag;
+
+  // For IFFT we have to delay a_p_b_real and a_p_b_imag before outputting the final result (pipelining doesn't work without it)
+  logic [`OVERALL_BITS-1:0] a_p_b_real, a_p_b_imag;
+  logic [`OVERALL_BITS-1:0] a_p_b_real_15D, a_p_b_imag_15D;
+  DelayRegister #(.BITWIDTH(`OVERALL_BITS), .CYCLE_COUNT(1+15)) a_p_b_real_delay(.clk(clk), .in(a_p_b_real), .out(a_p_b_real_15D));
+  DelayRegister #(.BITWIDTH(`OVERALL_BITS), .CYCLE_COUNT(1+15)) a_p_b_imag_delay(.clk(clk), .in(a_p_b_imag), .out(a_p_b_imag_15D));
+
+  assign a_out_real = use_ct ? a_p_b_real : a_p_b_real_15D;
+  assign a_out_imag = use_ct ? a_p_b_imag : a_p_b_imag_15D;
+
+  logic [`OVERALL_BITS-1:0] b_real, b_imag;
+  assign b_real = use_ct ? mul_out_real_1DP : b_in_real;
+  assign b_imag = use_ct ? mul_out_imag_1DP : b_in_imag;
+
   FFTButterflyAddStage add_stage(
-    .clk(clk),
-    .start(start),
-    .a_real(a_in_real),
-    .a_imag(a_in_imag),
-    .b_real(mul_out_real_1DP),
-    .b_imag(mul_out_imag_1DP),
+                         .clk(clk),
+                         .start(start),
 
-    .a_p_b_real(a_out_real),
-    .a_p_b_imag(a_out_imag),
-    .a_m_b_real(a_m_b_real),
-    .a_m_b_imag(a_m_b_imag),
+                         .a_real(a_real),
+                         .a_imag(a_imag),
+                         .b_real(b_real),
+                         .b_imag(b_imag),
 
-    .done(add_done)
-  );
+                         .a_p_b_real(a_p_b_real),
+                         .a_p_b_imag(a_p_b_imag),
+                         .a_m_b_real(a_m_b_real),
+                         .a_m_b_imag(a_m_b_imag),
+
+                         .done(add_done)
+                       );
 
   ///////////////////// Mult stage ///////////////////////////
   logic [`OVERALL_BITS-1:0] mul_out_real, mul_out_imag;
   logic add_done_1DP, add_done_2DP;
-  always_ff @(posedge clk)begin
+  always_ff @(posedge clk) begin
     a_m_b_real_1DP <= use_ct ? b_in_real : a_m_b_real;
     a_m_b_imag_1DP <= use_ct ? b_in_imag : a_m_b_imag;
-    mul_out_real_1DP <= use_ct ? mul_out_real : b_in_real;
-    mul_out_imag_1DP <= use_ct ? mul_out_imag : b_in_imag;
+    mul_out_real_1DP <= mul_out_real;
+    mul_out_imag_1DP <= mul_out_imag;
     add_done_1DP <= add_done;
     add_done_2DP <= add_done_1DP;
   end
 
   ComplexMultiplier tw_factor_mult(
-    .clk(clk),
-    .start(add_done_2DP),
-    .a_real(a_m_b_real_1DP),
-    .a_imag(a_m_b_imag_1DP),
-    .b_real(tw_real),
-    .b_imag(tw_imag),
+                      .clk(clk),
+                      .start(add_done_2DP),
+                      .a_real(a_m_b_real_1DP),
+                      .a_imag(a_m_b_imag_1DP),
+                      .b_real(tw_real),
+                      .b_imag(tw_imag),
 
-    .a_x_b_real(mul_out_real),
-    .a_x_b_imag(mul_out_imag),
+                      .a_x_b_real(mul_out_real),
+                      .a_x_b_imag(mul_out_imag),
 
-    .done(done)
-  );
+                      .done(done)
+                    );
 
-  assign b_out_real = use_ct ? a_m_b_real : mul_out_real;
-  assign b_out_imag = use_ct ? a_m_b_imag : mul_out_imag;
+  logic [`OVERALL_BITS-1:0] b_out_real_0DP, b_out_imag_0DP;
+  assign b_out_real_0DP = use_ct ? a_m_b_real : mul_out_real;
+  assign b_out_imag_0DP = use_ct ? a_m_b_imag : mul_out_imag;
+
+  logic [`OVERALL_BITS-1:0] b_out_real_1DP, b_out_imag_1DP;
+  DelayRegister #(.BITWIDTH(`OVERALL_BITS), .CYCLE_COUNT(1+0)) b_out_real_delay(.clk(clk), .in(b_out_real_0DP), .out(b_out_real_1DP));
+  DelayRegister #(.BITWIDTH(`OVERALL_BITS), .CYCLE_COUNT(1+0)) b_out_imag_delay(.clk(clk), .in(b_out_imag_0DP), .out(b_out_imag_1DP));
+  assign b_out_real = use_ct ? b_out_real_0DP : b_out_real_1DP;
+  assign b_out_imag = use_ct ? b_out_imag_0DP : b_out_imag_1DP;
 
 endmodule
