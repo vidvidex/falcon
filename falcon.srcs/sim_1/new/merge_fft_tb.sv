aedec04c89908e5785d0467e04c305811596ec78
Vid Smole <vid@smole.org>
2025-06-05 17:02:19 +0200

    Add merge_fft

---
+++ falcon.srcs/sim_1/new/merge_fft_tb.sv
@@ -0,0 +1,226 @@
+`timescale 1ns / 1ps
+
+module merge_fft_tb;
+
+  parameter int N = 1024;
+
+  logic clk, rst;
+  logic mode;
+  logic start;
+  logic done;
+  logic [$clog2(N):0] size;
+
+  logic [$clog2(N)-2:0] bram1_addr_a, bram1_addr_b;
+  logic [127:0] bram1_din_a, bram1_din_b;
+  logic [127:0] bram1_dout_a, bram1_dout_b;
+  logic bram1_we_a, bram1_we_b;
+  fft_bram_1024 fft_bram_1024_1 (
+                  .addra(bram1_addr_a),
+                  .clka(clk),
+                  .dina(bram1_din_a),
+                  .douta(bram1_dout_a),
+                  .wea(bram1_we_a),
+
+                  .addrb(bram1_addr_b),
+                  .clkb(clk),
+                  .dinb(bram1_din_b),
+                  .doutb(bram1_dout_b),
+                  .web(bram1_we_b)
+                );
+
+  logic [$clog2(N)-2:0] bram2_addr_a, bram2_addr_b;
+  logic [127:0] bram2_din_a, bram2_din_b;
+  logic [127:0] bram2_dout_a, bram2_dout_b;
+  logic bram2_we_a, bram2_we_b;
+  fft_bram_1024 fft_bram_1024_2 (
+                  .addra(bram2_addr_a),
+                  .clka(clk),
+                  .dina(bram2_din_a),
+                  .douta(bram2_dout_a),
+                  .wea(bram2_we_a),
+
+                  .addrb(bram2_addr_b),
+                  .clkb(clk),
+                  .dinb(bram2_din_b),
+                  .doutb(bram2_dout_b),
+                  .web(bram2_we_b)
+                );
+
+  merge_fft #(
+              .N(N)
+            )merge_fft(
+              .clk(clk),
+              .rst(rst),
+              .size(size),
+              .start(start),
+
+              .bram1_addr_a(bram1_addr_a),
+              .bram1_din_a(bram1_din_a),
+              .bram1_dout_a(bram1_dout_a),
+              .bram1_we_a(bram1_we_a),
+              .bram1_addr_b(bram1_addr_b),
+              .bram1_din_b(bram1_din_b),
+              .bram1_dout_b(bram1_dout_b),
+              .bram1_we_b(bram1_we_b),
+
+              .bram2_addr_a(bram2_addr_a),
+              .bram2_din_a(bram2_din_a),
+              .bram2_dout_a(bram2_dout_a),
+              .bram2_we_a(bram2_we_a),
+              .bram2_addr_b(bram2_addr_b),
+              .bram2_din_b(bram2_din_b),
+              .bram2_dout_b(bram2_dout_b),
+              .bram2_we_b(bram2_we_b),
+
+              .done(done)
+            );
+
+  // Signals that split 128 bit BRAM line into real and imag part, used mostly for debugging
+  logic [63:0] bram1_out_a_real, bram1_out_a_imag;
+  logic [63:0] bram1_out_b_real, bram1_out_b_imag;
+  logic [63:0] bram2_out_a_real, bram2_out_a_imag;
+  logic [63:0] bram2_out_b_real, bram2_out_b_imag;
+  assign {bram1_out_a_real, bram1_out_a_imag} = bram1_dout_a;
+  assign {bram1_out_b_real, bram1_out_b_imag} = bram1_dout_b;
+  assign {bram2_out_a_real, bram2_out_a_imag} = bram2_dout_a;
+  assign {bram2_out_b_real, bram2_out_b_imag} = bram2_dout_b;
+  logic [63:0] bram1_in_a_real, bram1_in_a_imag;
+  logic [63:0] bram1_in_b_real, bram1_in_b_imag;
+  logic [63:0] bram2_in_a_real, bram2_in_a_imag;
+  logic [63:0] bram2_in_b_real, bram2_in_b_imag;
+  assign {bram1_in_a_real, bram1_in_a_imag} = bram1_din_a;
+  assign {bram1_in_b_real, bram1_in_b_imag} = bram1_din_b;
+  assign {bram2_in_a_real, bram2_in_a_imag} = bram2_din_a;
+  assign {bram2_in_b_real, bram2_in_b_imag} = bram2_din_b;
+
+  // For result verification
+  real a_real_double, a_imag_double, b_real_double, b_imag_double;
+
+  function bit double_equal(real a, real b, real epsilon = 1e-6);
+    real diff;
+    diff = a - b;
+    if (diff < 0.0)
+      diff = -diff;
+    return (diff < epsilon);
+  endfunction
+
+  always #5 clk = ~clk;
+
+  initial begin
+    clk = 0;
+
+    mode = 0;
+    rst = 1;
+    start = 0;
+    #15;
+    rst = 0;
+
+    ////// Start BRAM 1 initialization for size 16 //////
+    size = 16;
+    bram1_we_a <= 1;
+    bram1_we_b <= 1;
+    for(int i = 0; i < (size>>2); i++) begin
+      real i_real = real'(i);
+      bram1_addr_a <= i;
+      bram1_din_a <= {$realtobits(i_real), $realtobits(i_real+(size>>2))};
+
+      bram1_addr_b <= i + (size>>2);
+      bram1_din_b <= {$realtobits(i_real+size/2), $realtobits(i_real + (size>>2) + (size>>1))};
+      #10;
+    end
+    bram1_we_a <= 0;
+    bram1_we_b <= 0;
+    ////// Finish BRAM 1 initialization //////
+
+    ////// Test for size 16 (8 real, 8 imaginary numbers) //////
+    start = 1;
+    #10;
+    start = 0;
+
+
+    // Wait for done signal
+    while(!done)
+      #10;
+
+    // Check if first and last value of f are correct
+    bram2_addr_a = 0;
+    bram2_addr_b = size/2-1;
+    #30;
+    a_real_double = $bitstoreal(bram2_out_a_real);
+    if(!double_equal(a_real_double, 5.505198))
+      $fatal(1, "merge_fft size 16: Expected first real part to be 5.505198, got %f", a_real_double);
+
+    a_imag_double = $bitstoreal(bram2_out_a_imag);
+    if(!double_equal(a_imag_double, 17.330145))
+      $fatal(1, "merge_fft size 16: Expected first imag part to be 17.330145, got %f", a_imag_double);
+
+    b_real_double = $bitstoreal(bram2_out_b_real);
+    if(!double_equal(b_real_double, 2.187387))
+      $fatal(1, "merge_fft size 16: Expected last real part to be 2.187387, got %f", b_real_double);
+
+    b_imag_double = $bitstoreal(bram2_out_b_imag);
+    if(!double_equal(b_imag_double, -11.583316))
+      $fatal(1, "merge_fft size 16: Expected last imag part to be -11.583316, got %f", b_imag_double);
+
+    $display("All tests for merge_fft with size 16 passed!");
+    ////// Finish test for size = 16 //////
+
+    rst = 1;
+    #10;
+    rst = 0;
+
+    ////// Start BRAM 1 initialization for size 1024 //////
+    size = 1024;
+
+    bram1_we_a <= 1;
+    bram1_we_b <= 1;
+    for(int i = 0; i < (size>>2); i++) begin
+      real i_real = real'(i);
+      bram1_addr_a <= i;
+      bram1_din_a <= {$realtobits(i_real), $realtobits(i_real+(size>>2))};
+
+      bram1_addr_b <= i + (size>>2);
+      bram1_din_b <= {$realtobits(i_real+size/2), $realtobits(i_real + (size>>2) + (size>>1))};
+      #10;
+    end
+    bram1_we_a <= 0;
+    bram1_we_b <= 0;
+    ////// Finish BRAM 1 initialization //////
+
+    ////// Test for size 1024 (512 real, 512 imaginary numbers) //////
+    size = 1024;
+    start = 1;
+    #10;
+    start = 0;
+
+
+    // Wait for done signal
+    while(!done)
+      #10;
+
+    // Check if first and last value of f are correct
+    bram2_addr_a = 0;
+    bram2_addr_b = size/2-1;
+    #30;
+    a_real_double = $bitstoreal(bram2_out_a_real);
+    if(!double_equal(a_real_double, 509.641399))
+      $fatal(1, "merge_fft size 1024: Expected first real part to be 509.641399, got %f", a_real_double);
+
+    a_imag_double = $bitstoreal(bram2_out_a_imag);
+    if(!double_equal(a_imag_double, 1025.567179))
+      $fatal(1, "merge_fft size 1024: Expected first imag part to be 1025.567179, got %f", a_imag_double);
+
+    b_real_double = $bitstoreal(bram2_out_b_real);
+    if(!double_equal(b_real_double, -502.552072))
+      $fatal(1, "merge_fft size 1024: Expected last real part to be -502.552072, got %f", b_real_double);
+
+    b_imag_double = $bitstoreal(bram2_out_b_imag);
+    if(!double_equal(b_imag_double, -519.015950))
+      $fatal(1, "merge_fft size 1024: Expected last imag part to be -519.015950, got %f", b_imag_double);
+
+    $display("All tests for merge_fft with size 1024 passed!");
+    $finish;
+
+  end
+
+endmodule
---
+++ falcon.srcs/sources_1/new/merge_fft.sv
@@ -0,0 +1,182 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Implements merge_fft
+//
+// Works for all powers of 2 smaller or equal to N (parameter "size")
+//
+// In the reference C implementation the function has 3 parameters:
+// - f0 (input)
+// - f1 (input)
+// - f (output)
+// Because each of the input is half the size of output this module is designed so that both inputs are in the same BRAM.
+// BRAM 1 is always input and BRAM 2 is always output.
+// The first half of BRAM 1 is for f0 and the second half for f1. Half in this case refers to "size"/2, not "N"/2
+//
+//          ---------------------------------------------------------------------------------
+// BRAM 1:  |   0   |   1   | ...  | size/4-1     | size/4 | size/4 +1 | ... | size/2-1     |
+//          | f0[0] | f0[1] |      | f0[size/4-1] | f1[0]  | f1[1]     |     | f1[size/4-1] |   (total length = "size"/2)
+//          ---------------------------------------------------------------------------------
+//
+//          ---------------------------------------------------------------------------------
+// BRAM 2:  |   0   |   1   |   2  |   3   |      ...           | size/2-2    | size/2-1    |
+//          | f[0]  | f[1]  | f[2] | f[3]  |                    | f[size/2-2] | f[size/2-1] |   (total length = "size"/2)
+//          ---------------------------------------------------------------------------------
+//
+// When size is less than N/2 there will be empty space at the end of each BRAM
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module merge_fft#(
+    parameter int N //
+  )(
+    input logic clk,
+    input logic rst,
+
+    input logic [$clog2(N):0] size, // Size of input (number of total real and complex values together, size = 1024 means 512 real and 512 imaginary values). Must be <= N/2
+    input logic start,
+
+    output logic [$clog2(N)-2:0] bram1_addr_a,
+    output logic [$clog2(N)-2:0] bram1_addr_b,
+    output logic [127:0] bram1_din_a,
+    output logic [127:0] bram1_din_b,
+    input logic [127:0] bram1_dout_a,
+    input logic [127:0] bram1_dout_b,
+    output logic bram1_we_a,
+    output logic bram1_we_b,
+
+    output logic [$clog2(N)-2:0] bram2_addr_a,
+    output logic [$clog2(N)-2:0] bram2_addr_b,
+    output logic [127:0] bram2_din_a,
+    output logic [127:0] bram2_din_b,
+    input logic [127:0] bram2_dout_a,
+    input logic [127:0] bram2_dout_b,
+    output logic bram2_we_a,
+    output logic bram2_we_b,
+
+    output logic done
+  );
+
+  logic [$clog2(N/4)-1:0] u, u_delayed; // Counter over the input. Will be at most N/4. We need the delayed version to know where to write the butterfly output
+  DelayRegister #(.BITWIDTH($clog2(N/4)), .CYCLE_COUNT(27)) u_delay(.clk(clk), .in(u), .out(u_delayed));
+
+  typedef enum logic [2:0] {
+            IDLE,
+            RUN,  // Run FFT
+            WAIT_FOR_BUTTERFLY_START, // Wait for the butterfly to start outputting data
+            WAIT_FOR_BUTTERFLY_END, // Wait for the butterfly to finish outputting all data
+            DONE  // FFT is done, immediately goes back to IDLE
+          } state_t;
+  state_t state, next_state;
+
+
+  logic [63:0] a_in_real, a_in_imag, b_in_real, b_in_imag;
+  logic [63:0] a_out_real, a_out_imag, b_out_real, b_out_imag;
+  logic butterfly_input_valid, butterfly_input_valid_delayed, butterfly_output_valid;
+  DelayRegister #(.BITWIDTH(1), .CYCLE_COUNT(3)) butterfly_input_valid_delay(.clk(clk), .in(butterfly_input_valid), .out(butterfly_input_valid_delayed));
+
+  FFTButterfly FFTButterfly(
+                 .clk(clk),
+                 .in_valid(butterfly_input_valid_delayed),
+                 .mode(1'b0),
+
+                 .a_in_real(a_in_real),
+                 .a_in_imag(a_in_imag),
+                 .b_in_real(b_in_real),
+                 .b_in_imag(b_in_imag),
+
+                 .tw_addr(tw_addr_delayed),
+
+                 .scale_factor(0),
+
+                 .a_out_real(a_out_real),
+                 .a_out_imag(a_out_imag),
+                 .b_out_real(b_out_real),
+                 .b_out_imag(b_out_imag),
+
+                 .done(butterfly_output_valid)
+               );
+
+  logic [9:0] tw_addr, tw_addr_delayed;
+  DelayRegister #(.BITWIDTH(10), .CYCLE_COUNT(3)) tw_addr_delay(.clk(clk), .in(tw_addr), .out(tw_addr_delayed));
+
+  // Reading inputs from BRAM 1
+  always @(posedge clk) begin
+    {a_in_real, a_in_imag} <= bram1_dout_a;
+    {b_in_real, b_in_imag} <= bram1_dout_b;
+  end
+
+  // Writing results to BRAM 2
+  always_ff @(posedge clk) begin
+    if (butterfly_output_valid) begin
+      bram2_addr_a <= u_delayed << 1;
+      bram2_addr_b <= (u_delayed << 1) + 1;
+      bram2_din_a <= {a_out_real, a_out_imag};
+      bram2_din_b <= {b_out_real, b_out_imag};
+      bram2_we_a <= 1'b1;
+      bram2_we_b <= 1'b1;
+    end
+    else begin
+      bram2_we_a <= 1'b0;
+      bram2_we_b <= 1'b0;
+    end
+  end
+
+  always_ff @(posedge clk) begin
+    if (rst)
+      state <= IDLE;
+    else
+      state <= next_state;
+  end
+
+  always_comb  begin
+    next_state = state;
+
+    case(state)
+      IDLE:
+        if(start)
+          next_state = RUN;
+      RUN:
+        if(u == (size >> 2) - 1)
+          next_state = WAIT_FOR_BUTTERFLY_START;
+      WAIT_FOR_BUTTERFLY_START:
+        if(butterfly_output_valid == 1'b1)  // Wait for butterfly unit to start outputting data
+          next_state = WAIT_FOR_BUTTERFLY_END;
+      WAIT_FOR_BUTTERFLY_END:
+        if(butterfly_output_valid == 1'b0)  // Wait for butterfly unit to finish outputting all data
+          next_state = DONE;
+      DONE:
+        next_state = IDLE;
+      default:
+        next_state = IDLE;
+    endcase
+  end
+
+  always_ff @(posedge clk) begin
+    if (rst) begin
+      u <= 0;
+
+      butterfly_input_valid <= 1'b0;
+    end
+    else if(state == RUN) begin
+
+      butterfly_input_valid <= 1'b1;
+
+      bram1_addr_a <= u  + 0;
+      bram1_addr_b <= u + (size >> 2);
+
+      bram1_we_a <= 1'b0;
+      bram1_we_b <= 1'b0;
+
+      tw_addr <= u + (size >> 1);
+
+      u <= u + 1;
+    end
+    else begin
+      butterfly_input_valid <= 1'b0;
+    end
+  end
+
+  assign done = state == DONE;
+
+endmodule
