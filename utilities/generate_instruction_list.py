dba9b6045f711ee349209a51c23aa92e8cb6e131
Vid Smole <vid@smole.org>
2025-09-08 18:39:12 +0200

    Update instruction generation

--- utilities/generate_instruction_list.py
+++ utilities/generate_instruction_list.py
@@ -26,26 +26,25 @@ class InstructionGenerator:
         bank4=0,
         bank5=0,
         bank6=0,
+        bank7=0,
         addr1=0,
         addr2=0,
-        mode=0,
-        mul_const_selection=0,
-        element_count=0,
-        bank7=0,
-        input_output_addr_same=0,
+        mode1=0,
         mode2=0,
+        element_count=0,
+        mul_const_constant=0,
+        input_output_addr_same=0,
     ):
         fields = [
             ("modules", 16, modules),
-            ("empty", 56, 0),
-            ("mode2", 1, mode2),
             ("input_output_addr_same", 1, input_output_addr_same),
-            ("bank7", 3, bank7),
+            ("mul_const_constant", 1, mul_const_constant),
             ("element_count", 4, element_count),
-            ("mul_const_selection", 1, mul_const_selection),
-            ("mode", 1, mode),
+            ("mode2", 1, mode2),
+            ("mode1", 1, mode1),
             ("addr2", 13, addr2),
             ("addr1", 13, addr1),
+            ("bank7", 3, bank7),
             ("bank6", 3, bank6),
             ("bank5", 3, bank5),
             ("bank4", 3, bank4),
@@ -57,8 +56,6 @@ class InstructionGenerator:
 
     def sel_module(
         self,
-        BRAM_READ=0,
-        BRAM_WRITE=0,
         COPY=0,
         HASH_TO_POINT=0,
         INT_TO_DOUBLE=0,
@@ -76,9 +73,7 @@ class InstructionGenerator:
         SAMPLERZ=0,
     ):
         modules = (
-            (BRAM_READ << 16)
-            | (BRAM_WRITE << 15)
-            | (COPY << 14)
+            (COPY << 14)
             | (HASH_TO_POINT << 13)
             | (INT_TO_DOUBLE << 12)
             | (FFT_IFFT << 11)
@@ -106,7 +101,7 @@ class InstructionGenerator:
             print(f"Error: Unsupported algorithm '{algorithm}'")
             return
 
-        formatted_instructions = [f"128'h{instruction:032x}" for instruction in self.instructions]
+        formatted_instructions = [f"70'b{instruction:070b}" for instruction in self.instructions]
 
         print(f"localparam int {algorithm.upper()}_INSTRUCTION_COUNT = {len(formatted_instructions)};")
         print(f"assign {algorithm}_instructions = '{{")
@@ -121,7 +116,7 @@ class InstructionGenerator:
         dprint("NTT DECOMPRESS HASH_TO_POINT")
         self.add_instruction(
             modules=self.sel_module(NTT_INTT=1, DECOMPRESS=1, HASH_TO_POINT=1),
-            mode=0,  # NTT mode: NTT
+            mode1=0,  # NTT mode1: NTT
             bank1=0,  # NTT bank1
             bank2=2,  # NTT bank2
             bank3=6,  # hash_to_point input
@@ -135,7 +130,7 @@ class InstructionGenerator:
         dprint("NTT")
         self.add_instruction(
             modules=self.sel_module(NTT_INTT=1),
-            mode=0,  # NTT mode: NTT
+            mode1=0,  # NTT mode1: NTT
             bank1=4,  # NTT bank1
             bank2=1,  # NTT bank2
         )
@@ -156,7 +151,7 @@ class InstructionGenerator:
         dprint("INTT")
         self.add_instruction(
             modules=self.sel_module(NTT_INTT=1),
-            mode=1,  # NTT mode: INTT
+            mode1=1,  # NTT mode1: INTT
             bank1=6,  # NTT bank1
             bank2=4,  # NTT bank2
         )
@@ -197,7 +192,7 @@ class InstructionGenerator:
             )
             self.add_instruction(
                 modules=self.sel_module(SAMPLERZ=1),
-                mode=1 if self.first_samplerz_call else 0,
+                mode1=1 if self.first_samplerz_call else 0,
                 mode2=1 if tree % 2 == 1 else 0,
                 bank1=prev_bram,  # Input
                 addr1=t0,
@@ -279,7 +274,7 @@ class InstructionGenerator:
         dprint(f"n={n},\tsub_z1\tin_bram={curr_bram}\tin_addr={z1}\tout_bram={next_bram}\tout_addr={tmp}")
         self.add_instruction(
             modules=self.sel_module(ADD_SUB=1),
-            mode2=1,  # Subtract mode
+            mode2=1,  # Subtract mode1
             bank3=curr_bram,  # Input1
             addr1=z1,
             bank4=next_bram,  # Input2, output
@@ -304,7 +299,7 @@ class InstructionGenerator:
         dprint(f"n={n},\tadd_t0\tin_bram={t0_bram}\tin_addr={t0}\tout_bram={next_bram}\tout_addr={tmp}")
         self.add_instruction(
             modules=self.sel_module(ADD_SUB=1),
-            mode2=0,  # Add mode
+            mode2=0,  # Add mode1
             bank3=t0_bram,  # Input1
             addr1=t0,
             bank4=next_bram,  # Input2, output
@@ -397,7 +392,7 @@ class InstructionGenerator:
         dprint("FFT")
         self.add_instruction(
             modules=self.sel_module(FFT_IFFT=1),
-            mode=0,  # FFT mode: FFT
+            mode1=0,  # FFT mode1: FFT
             bank1=5,  # FFT bank1
             bank2=4,  # FFT bank2
             addr1=0,  # Offset in bank1
@@ -440,7 +435,7 @@ class InstructionGenerator:
                 bank4=5,  # MUL_CONST output
                 addr1=0,  # Start offset for COMPLEX_MUL
                 addr2=0,  # End offset for COMPLEX_MUL
-                mul_const_selection=1,  # Select constant for multiplication
+                mul_const_constant=1,  # Select constant for multiplication
                 element_count=int(math.log2(N // 2)),
             )
         else:
@@ -452,7 +447,7 @@ class InstructionGenerator:
                 bank4=4,  # MUL_CONST output
                 addr1=0,  # Start offset for COMPLEX_MUL
                 addr2=0,  # End offset for COMPLEX_MUL
-                mul_const_selection=1,  # Select constant for multiplication
+                mul_const_constant=1,  # Select constant for multiplication
                 element_count=int(math.log2(N // 2)),
             )
 
@@ -463,7 +458,7 @@ class InstructionGenerator:
             modules=self.sel_module(MUL_CONST=1),
             bank3=timestep6_bank,  # MUL_CONST input
             bank4=timestep6_bank,  # MUL_CONST output
-            mul_const_selection=0,  # Select constant for multiplication
+            mul_const_constant=0,  # Select constant for multiplication
             element_count=int(math.log2(N // 2)),
         )
 
@@ -518,7 +513,7 @@ class InstructionGenerator:
         dprint("COMPLEX_MUL, ADD_SUB")
         self.add_instruction(
             modules=self.sel_module(COMPLEX_MUL=1, ADD_SUB=1),
-            mode2=0,  # Add mode
+            mode2=0,  # Add mode1
             bank1=4,  # COMPLEX_MUL input1, output
             bank2=1,  # COMPLEX_MUL input2
             addr1=N // 2,  # Offset for COMPLEX_MUL input1 and output, both inputs and output for ADD_SUB
@@ -542,7 +537,7 @@ class InstructionGenerator:
         dprint("IFFT, ADD_SUB")
         self.add_instruction(
             modules=self.sel_module(FFT_IFFT=1, ADD_SUB=1),
-            mode=1,  # IFFT mode: IFFT
+            mode1=1,  # IFFT mode1: IFFT
             mode2=0,  # add
             bank1=2,  # FFT bank1
             bank2=3,  # FFT bank2
@@ -556,7 +551,7 @@ class InstructionGenerator:
         dprint("IFFT")
         self.add_instruction(
             modules=self.sel_module(FFT_IFFT=1),
-            mode=1,  # IFFT mode: IFFT
+            mode1=1,  # IFFT mode1: IFFT
             bank1=5,  # FFT bank1
             bank2=4,  # FFT bank2
             addr1=N // 2,  # Offset for FFT bank1, ADD_SUB input
@@ -590,5 +585,7 @@ class InstructionGenerator:
 if __name__ == "__main__":
 
     generator = InstructionGenerator()
-    generator.verify(1024)
+    # generator.verify(512)
+    # generator.verify(1024)
+    generator.sign(512)
     # generator.sign(1024)
